4271df39e601cd06edd74dec9036493cb86ca8ce
LevinDmytro2 <46765662+LevinDmytro2@users.noreply.github.com>
2021-06-17 23:50:35 +0300

    Update test-azure.tf

--- test-azure.tf
+++ test-azure.tf
@@ -1,5 +1,29 @@
+provider "azurerm" {
+  features {}
+
+
+}
+
 resource "azurerm_resource_group" "k8srg" {
   name     = "k8srg-test-terraform"
   location = "West Europe"
 
+}
+
+terraform {
+  required_providers {
+    azuredevops = {
+      source = "microsoft/azuredevops"
+      version = ">=0.1.4"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=2.63.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+    
+  }
 }
